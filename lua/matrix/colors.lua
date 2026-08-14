6613a5c54d8a307161af064d98e9ec3d821f9ac7
homelessananpat <ananpatneo11@gmail.com>
2026-08-14 19:51:12 +0700

    Update colors.lua

--- lua/matrix/colors.lua
+++ lua/matrix/colors.lua
@@ -1,14 +1,14 @@
 local matrix = (vim.o.background == "dark")
 		and {
 			--16 colors
-            matrix0_gui = "#0D0208", -- matrix0 in palette
+            matrix0_gui = "#000000", -- matrix0 in palette
             matrix1_gui = "#113311",
             matrix2_gui = "#003B00",
             matrix3_gui = "#226622",
 			matrix3_gui_bright = "#616E88", -- out of palette
-            matrix4_gui = "#55ff55",
-            matrix5_gui = "#00FF41",
-            matrix6_gui = "#00FF41",
+            matrix4_gui = "#00FF00",
+            matrix5_gui = "#00FF00",
+            matrix6_gui = "#00FF00",
             matrix7_gui = "#339955",
             matrix8_gui = "#339933",
             matrix9_gui = "#008F11",
@@ -16,28 +16,28 @@ local matrix = (vim.o.background == "dark")
             matrix11_gui = "#ff0000",
             matrix12_gui = "#D08770",
             matrix13_gui = "#FFFF00",
-            matrix14_gui = "#00FF41",
+            matrix14_gui = "#00FF00",
             matrix15_gui = "#E4D00A",
 			none = "NONE",
 		}
 	or {
 		--16 colors
-        matrix0_gui = "#00FF41", -- matrix0 in palette
-        matrix1_gui = "#00FF41",
-        matrix2_gui = "#55ff55",
+        matrix0_gui = "#00FF00", -- matrix0 in palette
+        matrix1_gui = "#00FF00",
+        matrix2_gui = "#00FF00",
         matrix3_gui = "#226622",
 		matrix3_gui_bright = "#AEC7DF", -- out of palette
         matrix4_gui = "#003B00",
         matrix5_gui = "#113311",
-        matrix6_gui = "#0D0208",
+        matrix6_gui = "#000000",
         matrix7_gui = "#339955",
         matrix8_gui = "#339933",
         matrix9_gui = "#008F11",
         matrix10_gui = "#027c14",
-        matrix11_gui = "#ff0000",
+        matrix11_gui = "#FF0000",
         matrix12_gui = "#D08770",
         matrix13_gui = "#FFFF00",
-        matrix14_gui = "#00FF41",
+        matrix14_gui = "#00FF00",
         matrix15_gui = "#E4D00A",
 		none = "NONE",
 	}
